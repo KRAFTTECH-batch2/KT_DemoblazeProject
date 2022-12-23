f0c379bb5514ed22278d8a8dc5e16ec67644d24b
FTosun21 <tosunfatih9.2@gmail.com>
2022-12-23 21:46:09 +0300

    day2 purchase e2e test added

--- src/test/java/com/demoblaze/pages/ProductPage.java
+++ src/test/java/com/demoblaze/pages/ProductPage.java
@@ -1,19 +1,40 @@
 package com.demoblaze.pages;
 
+import com.demoblaze.utilities.BrowserUtils;
+import com.demoblaze.utilities.Driver;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ProductPage extends BasePage{
+public class ProductPage extends BasePage {
 
     @FindBy(id = "nameofuser")
     public WebElement welcomeUsername_loc;
+    @FindBy(xpath = "//a[.='Add to cart']")
+    public WebElement addToCart_loc;
 
-    public void verifyLogin_mtd(String expectedUsername){
+    public void verifyLogin_mtd(String expectedUsername) {
         String actualUsername = welcomeUsername_loc.getText();
-        Assert.assertEquals("user does NOT match",expectedUsername,actualUsername);
+        Assert.assertEquals("user does NOT match", expectedUsername, actualUsername);
         System.out.println("expectedUsername = " + expectedUsername);
         System.out.println("actualUsername = " + actualUsername);
+    }
+
+    public void addProduct_mtd(String product, String category) {
+        WebElement categoryModul = Driver.get().findElement(By.xpath("//a[.='" + category + "']"));
+        BrowserUtils.waitForClickablility(categoryModul, 5).click();
+        WebElement productItem = Driver.get().findElement(By.xpath("//a[.='" + product + "']"));
+        BrowserUtils.scrollToElement(productItem);
+        BrowserUtils.waitForClickablility(productItem, 5).click();
+        BrowserUtils.waitFor(2);
+        addToCart_loc.click();
+        BrowserUtils.waitFor(2);
+        Alert alert = Driver.get().switchTo().alert();
+        alert.accept();
+        BrowserUtils.waitForClickablility(homeLink_loc, 5).click();
+        BrowserUtils.waitFor(2);
     }
 
 }
